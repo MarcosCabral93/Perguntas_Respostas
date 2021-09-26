de0d571901b01f097fed0da254714b34dfde15a8
Marcos Paulo M Cabral <69735297+MarcosCabral93@users.noreply.github.com>
2021-09-26 18:13:15 -0300

    feat: Este commit estilizara a aplicacao

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express=require('express');
 const app=express();
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
 app.get("/", (req,res)=>{
 res.render("index",{
     nome: "Marcos Paulo",
     linguagem: "python"
 })
 })
+
+app.get("/perguntas", (req,res)=>{
+    res.render("perguntar")
+    })
+
+
 app.listen(3000,()=>{
     console.log("App Rodando na porta 3000")
 
